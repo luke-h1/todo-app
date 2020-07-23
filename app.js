69a61609794441ea80ae1ac2bc189f126d6a3669
luke-h1 <luke.howsam@yahoo.com>
2020-07-23 17:47:57 +0100

    more improvements

--- app.js
+++ app.js
@@ -14,13 +14,9 @@ function deleteOrTick(e) {
     }
 }
 
-
-
-
-
 // tick an individual task:
-function tickTask(e) { 
-    const task = document.querySelector('.form-check-label')
+function tickTask(e) {
+    const task = document.querySelector('.form-check-label');
     if (task.target.checked) {
         task.style.textDecoration = 'line-through';
         task.style.color = 'red'; // change to better color maybe ???
@@ -41,21 +37,23 @@ function submit(e) {
     e.preventDefault();
     const input = document.querySelector('input');
     if (input.value != '') addTask(input.value); // as long as the user enters something, then submit it.
-    input.value = '' //clear out input value after user submits it 
+    input.value = ''; //clear out input value after user submits it
 }
 
+// add todo tasks
 
-// add todo tasks 
-
-function addTask(task){
-    const ul = document.querySelector('ul'); 
-    const li = document.createElement('li'); 
-    li.innerHTML = `
-    <li class="collection-item">
+function addTask(task) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerHTML = `<li class="collection-item">
                 ${task}
                 <a href="#" class="delete-item secondary-content">
                 <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                <label class="form-check-label" for="inlineCheckbox1"></label>`
-    ul.appendChild(li) 
-    document.getElementsByClassName('card-action').style.display = 'block'  
+                <label class="form-check-label" for="inlineCheckbox1"></label>
+                </a> 
+                `
+
+
+    ul.appendChild(li);
+    document.getElementsByClassName('card-action').style.display = 'block';
 }
