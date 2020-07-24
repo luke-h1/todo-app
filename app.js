820de8913657c156865a73ec64ce27fbb9575875
luke-h1 <luke.howsam@yahoo.com>
2020-07-24 12:29:24 +0100

    add comments on how to fix

--- app.js
+++ app.js
@@ -46,20 +46,41 @@ function submit(e) {
     }
 }
 
+// function addTask(task) {
+//     const ul = document.querySelector('ul');
+//     const li = document.createElement('li');
+//     li.innerHTML = `<li class="collection-item">
+//                 ${task}
+//                 <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+//                 <label class="form-check-label" for="inlineCheckbox1"></label>
+//                 </a>
+//                 <button type="button" class="btn btn-danger clear-item">remove todo</button>
+//                 `;
+//     ul.appendChild(li);
+// }
+
+
+
+/*=====================
+Check box disabled  
+Need to learn how to 
+increment
+the value of the input 
+attributes 
+and values... 
+========================*/ 
 function addTask(task) {
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerHTML = `<li class="collection-item">
-                ${task}
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                <label class="form-check-label" for="inlineCheckbox1"></label> 
-                </a> 
-                <button type="button" class="btn btn-danger clear-item">remove todo</button>
-                `;
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `<li class="collection-item"> ${task} 
+    <button type="button" class="btn btn-danger clear-item">remove todo</button>`;
     ul.appendChild(li);
-    document.querySelector('card-collections').style.display = 'block';
-
-
 }
+
+
+
+
+
+
 
 console.log('🚀 https://github.com/luke-h1/todo-app-v2 🚀');
